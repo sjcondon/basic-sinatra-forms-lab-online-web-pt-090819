db6fe09b6839d36f4b3a9712025bb65ae1d78629
Sarah Condon <s.j.condon01@gmail.com>
2019-12-28 18:44:09 -0600

    Done.

--- app.rb
+++ app.rb
@@ -2,5 +2,20 @@ require 'sinatra/base'
 
 class App < Sinatra::Base
 
+    get '/newteam' do
+        erb :newteam
+      end
+    
+      post '/team' do
+        @name = "name"
+        @coach = "coach"
+        @pg = "pg"
+        @sg = "sg"
+        @sf = "sf"
+        @pf = "pf"
+        @center = "c"
+    
+        erb :team
+    end
 
 end
